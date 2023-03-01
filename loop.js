41f736d0c81f922b60c8c33493ccc9811a2eca54
Boyan Velikov <boyanvelikov@gmail.com>
2023-03-01 09:09:47 +0200

    Update

--- loop.js
+++ loop.js
@@ -1,4 +1,15 @@
 for (let i = 1; i <= 10; i++) {
     console.log(i);
   }
+
+function countTrue(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      count++;
+    }
+  }
+  return count;
+}
+
   
